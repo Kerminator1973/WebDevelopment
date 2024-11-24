c0b83fa4a5f864562f5b176653a1dff802c691e4
Maksim Rozhkov <kermit@mail.ru>
2024-11-24 11:50:37 +0300

    Blazor Example: добавлен диалог

---
+++ blazor_examples/SelectModel/Models/SimplePerson.cs
@@ -0,0 +1,9 @@
+﻿namespace SelectModel.Models
+{
+    public class SimplePerson
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
